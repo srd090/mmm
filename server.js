8e1d6bed383e57cf361330e1f895e59b8369669b
srd090 <ehospitalinfo2@gmail.com>
2025-06-03 16:25:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,19 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb+srv://mkasigiven09:yGiI6nmIj33vDrhk@srd-sassa-gov-za.qnutzho.mongodb.net/sassa?retryWrites=true&w=majority';
+/*const uri = 'mongodb+srv://mkasigiven09:yGiI6nmIj33vDrhk@srd-sassa-gov-za.qnutzho.mongodb.net/sassa' ;*/
 
-const client = new MongoClient(uri, {
-  serverApi: { version: '1' },
-});
-
+const uri = 'mongodb+srv://mkasigiven09:yGiI6nmIj33vDrhk@srd-sassa-gov-za.qnutzho.mongodb.net/?retryWrites=true&w=majority&appName=srd-sassa-gov-za//sassa'
+const client = new MongoClient(uri);
 let collection;
 
 async function startServer() {
   try {
     await client.connect();
-    console.log('Connected to MongoDB');
-
     const db = client.db('sassa');
     collection = db.collection('srd');
 
@@ -38,19 +34,15 @@ async function startServer() {
 
         res.status(200).send('Application submitted');
       } catch (err) {
-        console.error('Error in /submit:', err);
+        console.error(err);
         res.status(500).send('Server error');
       }
     });
 
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (err) {
     console.error('Failed to connect to DB:', err);
-    process.exit(1);
   }
 }
 
