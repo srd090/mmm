51afcc2da0a9a6a1af295c56d62539fb6542949e
srd090 <ehospitalinfo2@gmail.com>
2025-06-16 16:58:04 +0200

    Update Application.js

--- models/Application.js
+++ models/Application.js
@@ -1,10 +1,14 @@
+// models/Application.js
 const mongoose = require('mongoose');
 
 const applicationSchema = new mongoose.Schema({
-  idNumber: String,
-  phoneNumber: String,
-  newphoneNumber: String,
+  idNumber: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  newphoneNumber: { type: String, required: true },
   submittedAt: { type: Date, default: Date.now }
 });
+
+// Optional: Index for faster duplicate checking
+applicationSchema.index({ idNumber: 1, submittedAt: 1 });
 
 module.exports = mongoose.model('Application', applicationSchema);
